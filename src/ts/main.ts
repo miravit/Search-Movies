0772e15f63bc2b5e2591485ee8223df094612dbc
MAC <miravitsas1@gmail.com>
2022-11-16 17:08:48 +0100

    test

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,5 @@
 import { IMovie } from "./models/IMovie";
-import { searchMovies } from "./services/movieService";
+import { movieSearch } from "./services/movieService";
 
 (document.getElementById("searchForm") as HTMLFormElement).addEventListener(
   "submit",
@@ -7,29 +7,35 @@ import { searchMovies } from "./services/movieService";
     //I min klickhändelse när jag submittar så anropas en asyncfunktion. e är ett inbyggt objekt som har alla
     e.preventDefault();
 
-    let searchText: string = (
-      document.getElementById("searchText") as HTMLInputElement
-    ).value;
+    let theSearchText: HTMLInputElement = document.getElementById(
+      "searchText"
+    ) as HTMLInputElement;
+    let searchText: string = theSearchText.value;
 
-    let movies: IMovie[] = await searchMovies(searchText);
+    let movies: IMovie[] = await movieSearch(searchText);
     createHTML(movies);
   }
 );
 
 const createHTML = (movies: IMovie[]) => {
   let container = document.getElementById("searchResult") as HTMLDivElement;
+  container.className = "container";
   container.innerHTML = " ";
 
   for (let i = 0; i < movies.length; i++) {
-    let title = document.createElement("h3");
-    let img = document.createElement("img");
+    let frame: HTMLDivElement = document.createElement("div");
+    let title: HTMLHeadingElement = document.createElement("h3");
+    let img: HTMLImageElement = document.createElement("img");
+
+    frame.className = "frame";
 
     title.innerHTML = movies[i].Title;
     img.src = movies[i].Poster;
     img.alt = movies[i].Title;
 
-    container.appendChild(title);
-    container.appendChild(img);
+    frame.appendChild(title);
+    frame.appendChild(img);
+    container.appendChild(frame);
 
     document.body.appendChild(container);
   }
